d818966ed50b93b7848f86ef7e7f916e4d589f7b
nat06-dot <suphachokchosanthia@gmail.com>
2026-03-22 07:33:16 +0700

    finish

---
+++ interfaces.ts
@@ -0,0 +1,24 @@
+export interface VenueItem {
+  _id: string;
+  name: string;
+  address: string;
+  district: string;
+  province: string;
+  postalcode: string;
+  tel: string;
+  picture: string;
+}
+
+export interface VenueJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: VenueItem[];
+}
+
+export interface BookingItem {
+  nameLastname: string;
+  tel: string;
+  venue: string;
+  bookDate: string;
+}
