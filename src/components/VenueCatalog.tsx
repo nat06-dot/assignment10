38ffb8405d86b4b9acea07347c69bf1aba077459
nat06-dot <suphachokchosanthia@gmail.com>
2026-03-22 07:41:03 +0700

    finish

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,24 +1,21 @@
-import { VenueJson, VenueItem } from "../../interface";
+import { VenueJson, VenueItem } from "../../interfaces";
 import Card from "./Card";
 import Link from "next/link";
 
 export default async function VenueCatalog({
-    venuesJson,
+  venuesJson,
 }: {
-    venuesJson: Promise<VenueJson>;
+  venuesJson: Promise<VenueJson>;
 }) {
-    const venueJsonReady = await venuesJson;
+  const venueJsonReady = await venuesJson;
 
-    return (
-        <div className="flex flex-row flex-wrap justify-center gap-4">
-            {venueJsonReady.data.map((venue: VenueItem) => (
-                <Link key={venue._id} href={`/venue/${venue._id}`}>
-                    <Card
-                        venueName={venue.name}
-                        imgSrc={venue.picture}
-                    />
-                </Link>
-            ))}
-        </div>
-    );
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-4">
+      {venueJsonReady.data.map((venue: VenueItem) => (
+        <Link key={venue._id} href={`/venue/${venue._id}`}>
+          <Card venueName={venue.name} imgSrc={venue.picture} />
+        </Link>
+      ))}
+    </div>
+  );
 }
